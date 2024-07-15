9151571f9c181398ba3849d2ca479627ed0fc9fb
Ahmed12590 <muhammadahmed12590@gmail.com>
2024-07-15 16:07:27 +0500

    footer complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,12 @@
 import "./style.css";
 import { NavLink } from "react-router-dom";
 import Image from "./img/image-29 1.png";
-import svg from "./img/facebook.svg";
+import fb from "./img/facebook.svg";
+import instagram from "./img/instagram.svg.svg";
+import twitter from "./img/twitter.svg.svg";
+import location from "./img/location.svg";
+import email from "./img/email.svg";
+import phone from "./img/phone.svg";
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 
@@ -41,31 +46,57 @@ const Footer = () => {
           <Col sm={12} md={12} lg={3}>
             <h4 className="head">Social Links </h4>
             <div className="social-parent">
-            <div className="social-links">
-              <img src={svg} alt="facebook.svg" />
+              <div className="social-links">
+                <img src={fb} alt="facebook.svg" />
+              </div>
+              <div className="social-text">
+                <div>Lorem Ipsum</div>
+              </div>
             </div>
-            <div className="social-text">
-              <div>Lorem Ipsum</div>
+            <div className="social-parent-instagram">
+              <div className="social-instagram">
+                <img src={instagram} alt="instagram.svg.svg" />
+              </div>
+              <div className="social-text">
+                <div>Lorem Ipsum</div>
+              </div>
             </div>
+            <div className="social-parent-twitter">
+              <div className="social-twitter">
+                <img src={twitter} alt="twitter.svg.svg" />
+              </div>
+              <div className="social-text">
+                <div>Lorem Ipsum</div>
+              </div>
             </div>
           </Col>
 
           <Col sm={12} md={12} lg={3}>
             <h4 className="head">Contact us </h4>
-            <ul>
-              <li className="contact-link">
-                <NavLink to={`/`}>Home</NavLink>
-              </li>
-              <li className="footer-link">
-                <NavLink to={`/`}>Home</NavLink>
-              </li>
-              <li className="footer-link">
-                <NavLink to={`/`}>Home</NavLink>
-              </li>
-              <li className="footer-link">
-                <NavLink to={`/`}>Home</NavLink>
-              </li>
-            </ul>
+            <div className="contact-parent">
+              <div className="contact-location">
+                <img src={location} alt="location.svg" />
+              </div>
+              <div className="contact-location-text">
+                <div>Lorem Ipsum</div>
+              </div>
+            </div>
+            <div className="contact-parent-email">
+              <div className="contact-emial">
+                <img src={email} alt="email.svg" />
+              </div>
+              <div className="contact-email-text">
+                <div>Lorem Ipsum</div>
+              </div>
+            </div>
+            <div className="contact-parent-phone">
+              <div className="contact-phone">
+                <img src={phone} alt="phone.svg" />
+              </div>
+              <div className="contact-phone-text">
+                <div>+123 456 7890</div>
+              </div>
+            </div>
           </Col>
         </Row>
       </Container>
