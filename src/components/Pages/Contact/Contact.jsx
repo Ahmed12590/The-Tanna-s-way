6671300fafa372904081fa90221b2d3870c45a3b
Ahmed12590 <muhammadahmed12590@gmail.com>
2024-07-15 22:55:15 +0500

    contact page  newsletter

--- src/components/Pages/Contact/Contact.jsx
+++ src/components/Pages/Contact/Contact.jsx
@@ -7,22 +7,46 @@ const Contact = () => {
         <h1 className="contact-banner-heading">CONTACT US </h1>
       </section>
 
+      <section className="Newsletter">
+        <div className="Newsletter-background">
+          <div className="Newsletter-col">
+            <h1 className="Newsletter-heading">Subscribe Our Newsletter</h1>
+            <p className="Newsletter-para">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              praesent duis elementum eu risus nec.{" "}
+            </p>
+
+            <form action="">
+              <div
+                className="mb-3 text-center d-flex align-items-center justify-content-center position-relative"
+                style={{ maxWidth: "39.5625rem", margin: "auto" }}
+              >
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Enter Your Email Adress"
+                />
+                <button className="btn-subscribe">Subscribe</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+
       <section className="box-sec">
         <Container fluid>
           <Row>
-            <Col xs={12} md={12} lg={6}>
+            <Col xs={12} md={12} lg={6} className="contact-col">
               <div className="contact-background">
                 <div className="contact-box"></div>
               </div>
             </Col>
 
             <Col xs={12} md={12} lg={6} className="form-col">
-            <div>
-              <h1 className="form-heading">
-              Get in Touch
-              </h1>
-
-            </div>
+              <div>
+                <h1 className="form-heading">Get in Touch</h1>
+              </div>
               <form>
                 <div class="form-group">
                   <input
