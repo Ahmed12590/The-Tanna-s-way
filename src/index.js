e010b059309eb1d8740c5a2f0dd63056c03fd2d0
Ahmed12590 <muhammadahmed12590@gmail.com>
2024-07-23 18:45:17 +0500

    ScrollRestoration

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ScrollTop from './components/ScrollToTop';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+  <ScrollTop/>
     <App />
   </BrowserRouter>
 );
